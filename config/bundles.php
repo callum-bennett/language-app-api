89d5c0c77ae552c2cbe4d24d650351d519fc9228
Callum Bennett <callum.bennett@enovation.ie>
2020-09-27 15:20:50 +0100

    composer update and allow fixtures to run on heroku

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];

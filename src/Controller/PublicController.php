fdf38e1d623bd3930be33869dd6fb04b03b62cef
Callum Bennett <callum.bennett@enovation.ie>
2020-10-03 12:41:09 +0100

    Add route to serve public images

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PublicController
+ * @package App\Controller
+ */
+class PublicController extends AbstractController
+{
+    /**
+     * @Route("/image/{filename}", name="image")
+     * @param $filename
+     * @return Response
+     */
+    public function photo($filename): Response
+    {
+        $publicImageDir = $this->getParameter('publicImageDir');
+        return new BinaryFileResponse("{$publicImageDir}/{$filename}");
+    }
+}

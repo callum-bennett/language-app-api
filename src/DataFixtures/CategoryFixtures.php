a5fec689f18beb0e0a2bf9305893fa4f62124fd2
Callum Bennett <callumbennett.cb@gmail.com>
2020-10-03 11:41:22 +0100

    Add fileuploader service, add images to category fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -3,23 +3,51 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Service\FileUploader;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class CategoryFixtures extends Fixture implements FixtureInterface
+class CategoryFixtures extends Fixture implements FixtureInterface, ContainerAwareInterface
 {
     public const CAT_REF_FAMILY = 'family';
     public const CAT_REF_TOP_100_WORDS = 'top100words';
     public const CAT_REF_TRAVEL = 'travel';
     public const CAT_REF_NUMBERS = 'numbers';
 
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    private $fileUploader;
+
+    /**
+     * CategoryFixtures constructor.
+     *
+     * @param FileUploader $fileUploader
+     */
+    public function __construct(FileUploader $fileUploader) {
+        $this->fileUploader = $fileUploader;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $objectManager): void
     {
         foreach ($this->getCategories() as [$name, $imagePath, $ref]) {
+            $directory = $this->container->getParameter("publicImageDir");
+            $file = new UploadedFile($imagePath, str_replace(" ", "", $name), null, null, true);
+            $fileName = $this->fileUploader->upload($file, $directory);
+
             $category = new Category();
             $category->setName($name);
-            $category->setImageUrl($imagePath);
+            $category->setImageUrl($fileName);
 
             $objectManager->persist($category);
             $this->addReference($ref, $category);
@@ -32,10 +60,10 @@ class CategoryFixtures extends Fixture implements FixtureInterface
     {
         return [
             // $category = [$name, $imagePath, $ref];
-            ["Top 100 words", "/static/media/top100.d0695270.jpg", self::CAT_REF_TOP_100_WORDS],
-            ["Family", "/static/media/static/media/people.f3302419.jpg", self::CAT_REF_FAMILY],
-            ["Travel", "/static/media/top100.d0695270.jpg", self::CAT_REF_TRAVEL],
-            ["Numbers", "/static/media/top100.d0695270.jpg", self::CAT_REF_NUMBERS],
+            ["Top 100 words", realpath(__DIR__) . "/images/top100.jpg", self::CAT_REF_TOP_100_WORDS],
+            ["Family", realpath(__DIR__) . "/images/people.jpg", self::CAT_REF_FAMILY],
+            ["Travel", realpath(__DIR__) . "/images/travel.jpg", self::CAT_REF_TRAVEL],
+            ["Numbers", realpath(__DIR__) . "/images/numbers.jpg", self::CAT_REF_NUMBERS],
         ];
     }
 }
